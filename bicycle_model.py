2d0de7eadf7b4d7d913c4bba388dd4ed27fd8ca7
Uchiha729 <f20221736@hyderabad.bits-pilani.ac.in>
2024-07-13 16:15:39 +0530

    first commit

---
+++ bicycle_model.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+
+max_steer = np.radians(30.0)  # [rad] max steering angle
+L = 2.9  # [m] Wheel base of vehicle
+dt = 0.1
+Lr = L / 2.0  # [m]
+Lf = L - Lr
+Cf = 1600.0 * 2.0  # N/rad
+Cr = 1700.0 * 2.0  # N/rad
+Iz = 2250.0  # kg/m2
+m = 1500.0  # kg
+
+class LinearBicycleModel(object):
+    """
+    Class representing the state of a vehicle.
+    :param x: (float) x-coordinate
+    :param y: (float) y-coordinate
+    :param yaw: (float) yaw angle
+    :param v: (float) speed
+    """
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        """Instantiate the object."""
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+
+    def update(self, throttle, delta):
+        """
+        Update the state of the vehicle.
+        Stanley Control uses bicycle model.
+        :param a: (float) Acceleration
+        :param delta: (float) Steering
+        """
+        delta = np.clip(delta, -max_steer, max_steer)
+
+        self.x += self.v * np.cos(self.yaw) * dt
+        self.y += self.v * np.sin(self.yaw) * dt
+        self.yaw += self.v / L * np.tan(delta) * dt
+        self.yaw = normalize_angle(self.yaw)
+        self.v += throttle * dt
+
+
+def normalize_angle(angle):
+    """
+    Normalize an angle to [-pi, pi].
+    :param angle: (float)
+    :return: (float) Angle in radian in [-pi, pi]
+    """
+    while angle > np.pi:
+        angle -= 2.0 * np.pi
+
+    while angle < -np.pi:
+        angle += 2.0 * np.pi
+
+    return angle
